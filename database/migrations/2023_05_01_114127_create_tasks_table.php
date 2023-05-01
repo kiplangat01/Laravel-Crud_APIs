c3e76470c202c5a174fe95bad9f20c33cd334d45
kiplangat01 <meshnobie@gmail.com>
2023-05-01 15:29:22 +0300

    updated tasks table

--- database/migrations/2023_05_01_114127_create_tasks_table.php
+++ database/migrations/2023_05_01_114127_create_tasks_table.php
@@ -12,8 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
+            $table->string('name', 100);
+            $table->string('description', 255);
+            $table->timestamp('due_date')->nullable();
+            $table->unsignedSmallInteger('status_id');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
